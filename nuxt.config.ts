db32ec35826c0989582245b2995546bc0dbdaa8f
Yael Duckwen <yaelduckwen@gmail.com>
2024-02-16 13:56:58 -0300

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+	ssr: true,
+
 	app: {
         baseURL: '/dante/', // baseURL: '/<repository>/'
         buildAssetsDir: 'assets', // don't use "_" at the begining of the folder name to avoids nojkill conflict
